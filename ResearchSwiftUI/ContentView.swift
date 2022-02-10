fd63d2ac2f3636c826f221954e0b8aa862a75ff8
Ryan Pape <ryan.pape5@gmail.com>
2022-02-10 16:07:03 -0500

    Added Penn State Behrend logo to top of view

--- ResearchSwiftUI/ContentView.swift
+++ ResearchSwiftUI/ContentView.swift
@@ -3,7 +3,7 @@
 //  ResearchSwiftUI
 //
 //  Created by Elaine Lannon on 2/9/22.
-//
+//	Edit by Ryan Pape 2/10/22.
 //
 
 import SwiftUI
@@ -11,30 +11,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		VStack{
+			Image("Image").resizable().frame(width: 400, height: 125, alignment: .center);
             Button (action: {
                 //action
             }, label: {
-				Image("recognize").resizable().frame(width: 100, height: 100, alignment: .center);
-				Text("Recognize")
-            })
-            Button (action: {
-                //action
-            }, label: {
-				Image("respond").resizable().frame(width: 100, height: 100, alignment: .center);
-				Text("Respond    ")
-            })
-            Button (action: {
-                //action
-            }, label: {
-				Image("refer").resizable().frame(width: 100, height: 100, alignment: .center);
-				Text("Refer          ")
+				Image("recognize").resizable().frame(width: 150, height: 150, alignment: .center);
+				Text("Recognize").bold()
 				
             })
             Button (action: {
                 //action
             }, label: {
-				Image("resources").resizable().frame(width: 100, height: 100, alignment: .center);
-				Text("Resources")
+				Image("respond").resizable().frame(width: 150, height: 150, alignment: .center);
+				VStack{
+					Text("Respond    ").bold()
+					Text("")
+				}
+            })
+            Button (action: {
+                //action
+            }, label: {
+				Image("refer").resizable().frame(width: 150, height: 150, alignment: .center);
+				Text("Refer          ").bold()
+				
+            })
+            Button (action: {
+                //action
+            }, label: {
+				Image("resources").resizable().frame(width: 150, height: 150, alignment: .center);
+				Text("Resources").bold()
             })
         }
         
