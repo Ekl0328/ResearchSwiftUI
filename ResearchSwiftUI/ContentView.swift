70dafdc3d9d211bbb7cb897f77a30cd4eb665a20
Ryan Pape <ryan.pape5@gmail.com>
2022-02-17 10:17:06 -0500

    added descriptions to each button.

--- ResearchSwiftUI/ContentView.swift
+++ ResearchSwiftUI/ContentView.swift
@@ -10,36 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		VStack{
-			Image("Image").resizable().frame(width: 400, height: 125, alignment: .center);
+		VStack(alignment: .center, spacing: 5){
+			VStack(alignment: .center, spacing: 50){
+				Text("LionHelp").fontWeight(.heavy).bold().font(.system(size: 40)).frame(width: .infinity, height: .infinity, alignment: .center).background(Color.blue).frame(width: 500, height: 200, alignment: .center);
+			}
+			Image("Behrend").resizable().frame(width: 250, height: 75, alignment: .center);
             Button (action: {
                 //action
             }, label: {
-				Image("recognize").resizable().frame(width: 150, height: 150, alignment: .center);
-				Text("Recognize").bold()
+				Image("recognize").resizable().scaledToFit().frame(width: 100, height: 100, alignment: .center);
+				VStack(alignment: .leading, spacing: 5){
+					Text("Recognize").bold().frame(width: .infinity, height: .infinity, alignment: .leading);
+					Text("Common indicators of stress are listed in this section. Students may present with indicators not listed").font(.system(size: 12)).fontWeight(.light).frame(width: .infinity, height: .infinity, alignment: .leading).foregroundColor(.black);
+				}
 				
             })
             Button (action: {
                 //action
             }, label: {
-				Image("respond").resizable().frame(width: 150, height: 150, alignment: .center);
-				VStack{
-					Text("Respond    ").bold()
-					Text("")
+				Image("respond").resizable().frame(width: 100, height: 100, alignment: .center);
+				VStack(alignment: .leading, spacing: 5){
+					Text("Respond").bold().frame(width: .infinity, height: .infinity, alignment: .leading);
+					Text("Use the tips and listed pointers to determine the most approriate response.").font(.system(size: 12)).fontWeight(.light).frame(width: .infinity, height: .infinity, alignment: .leading).foregroundColor(.black);
+
 				}
             })
             Button (action: {
                 //action
             }, label: {
-				Image("refer").resizable().frame(width: 150, height: 150, alignment: .center);
-				Text("Refer          ").bold()
-				
+				Image("refer").resizable().frame(width: 100, height: 100, alignment: .center);
+				VStack(alignment: .leading, spacing: 5){
+					Text("Refer").bold().frame(width: .infinity, height: .infinity, alignment: .leading);
+					Text("Review the various options to help you determine what the appropriate next step would be in helping a distressed or disruptive student.").font(.system(size: 12)).fontWeight(.light).frame(width: .infinity, height: .infinity, alignment: .leading).foregroundColor(.black);
+				}
             })
             Button (action: {
                 //action
             }, label: {
-				Image("resources").resizable().frame(width: 150, height: 150, alignment: .center);
-				Text("Resources").bold()
+				Image("resources").resizable().frame(width: 100, height: 100, alignment: .center);
+				VStack(alignment: .leading, spacing: 5){
+					Text("Resources").bold();
+					Text("Browse the list of the emergency, urgent, and additional resources on campus.").font(.system(size: 12)).fontWeight(.light).frame(width: .infinity, height: .infinity, alignment: .leading).foregroundColor(.black);
+
+				}
             })
         }
         
@@ -48,6 +61,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+		Group {
+			ContentView()
+		}
     }
 }
+
